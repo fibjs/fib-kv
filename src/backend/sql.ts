816f3d92b7109ba267a3b099afe40cd8c6607d29
xicilion <lion@9465.net>
2025-04-19 03:50:34 +0800

    feat: update version to 1.6.0 and refactor SQL functions for better connection handling

--- src/backend/sql.ts
+++ src/backend/sql.ts
@@ -20,18 +20,20 @@ function exec(conn: FibKV.SqlConnection, sql: string, arg0?: any, arg1?: any, ar
     return conn.execute.apply(conn, params);
 }
 
-function sql(opts: FibKV.FibKVOptions & {
-    sqls?: Record<string, string >
+function sql(conn, opts: FibKV.FibKVOptions & {
+    sqls?: Record<string, string>
 }, method: string) {
     var sqls = opts.sqls;
 
     if (!sqls)
         sqls = opts.sqls = {
             get: `SELECT ${utils.value_name(opts)} FROM ${utils.table_name(opts)} WHERE ${utils.key_name(opts)} = ?${_v(opts, ` and _timestamp >= ?`)};`,
-            set: `INSERT INTO ${utils.table_name(opts)} (${utils.key_name(opts)}, ${utils.value_name(opts)}${_v(opts, `, _timestamp`)}) 
+            set: conn.type === 'psql' ?
+                `INSERT INTO ${utils.table_name(opts)} (${utils.key_name(opts)}, ${utils.value_name(opts)}${_v(opts, `, _timestamp`)}) 
                 VALUES (?, ?${_v(opts, `, ?`)}) 
                 ON CONFLICT (${utils.key_name(opts)}) DO UPDATE 
-                SET ${utils.value_name(opts)} = excluded.${utils.value_name(opts)}${_v(opts, `, _timestamp = excluded._timestamp`)};`,
+                SET ${utils.value_name(opts)} = excluded.${utils.value_name(opts)}${_v(opts, `, _timestamp = excluded._timestamp`)};`
+                : `REPLACE INTO ${utils.table_name(opts)} VALUES(?,?${_v(opts, `,?`)});`,
             has: `SELECT 1 FROM ${utils.table_name(opts)} WHERE ` +
                 `${utils.key_name(opts)} = ?${_v(opts, ` and _timestamp >= ?`)};`,
             keys: `SELECT ${utils.key_name(opts)} FROM ${utils.table_name(opts)}${_v(opts, ` WHERE _timestamp >= ?`)};`,
@@ -53,10 +55,10 @@ export = {
         conn.execute(_sql);
 
         if (utils.timeout(opts) > 0) {
-            var _mark = setInterval(()=>{
+            var _mark = setInterval(() => {
                 try {
-                    conn.execute(sql(opts, 'cleanup'), Date.now() - utils.timeout(opts));
-                } catch(e) {
+                    conn.execute(sql(conn, opts, 'cleanup'), Date.now() - utils.timeout(opts));
+                } catch (e) {
                     // 'Error: Invalid procedure call' arises when the connection is closed
                     clearInterval(_mark);
                 }
@@ -64,23 +66,23 @@ export = {
         }
     },
     get: (conn: FibKV.SqlConnection, opts: FibKV.FibKVOptions, k: string) => {
-        var rs = exec(conn, sql(opts, 'get'), k,
-                      utils.timeout(opts) > 0 ? Date.now() - utils.timeout(opts) : undefined);
+        var rs = exec(conn, sql(conn, opts, 'get'), k,
+            utils.timeout(opts) > 0 ? Date.now() - utils.timeout(opts) : undefined);
         return rs.length ? rs[0][utils.value_name(opts)] : null;
     },
-    set: (conn: FibKV.SqlConnection, opts: FibKV.FibKVOptions, k: string, v) => exec(conn, sql(opts, 'set'), k, v,
-                                    utils.timeout(opts) > 0 ? Date.now() : undefined),
-    has: (conn: FibKV.SqlConnection, opts: FibKV.FibKVOptions, k: string) => exec(conn, sql(opts, 'has'), k,
-                                 utils.timeout(opts) > 0 ? Date.now() - utils.timeout(opts) : undefined).length > 0,
+    set: (conn: FibKV.SqlConnection, opts: FibKV.FibKVOptions, k: string, v) => exec(conn, sql(conn, opts, 'set'), k, v,
+        utils.timeout(opts) > 0 ? Date.now() : undefined),
+    has: (conn: FibKV.SqlConnection, opts: FibKV.FibKVOptions, k: string) => exec(conn, sql(conn, opts, 'has'), k,
+        utils.timeout(opts) > 0 ? Date.now() - utils.timeout(opts) : undefined).length > 0,
     keys: (conn: FibKV.SqlConnection, opts: FibKV.FibKVOptions) => {
         var _keys = exec(
-            conn, sql(opts, 'keys'),
+            conn, sql(conn, opts, 'keys'),
             utils.timeout(opts) > 0 ? Date.now() - utils.timeout(opts) : undefined
         ).reduce((a, b) => a.concat(b instanceof Array ? b[0] : b[utils.key_name(opts)]), [])
 
         return _keys
     },
     renew: (conn: FibKV.SqlConnection, opts: FibKV.FibKVOptions, k: string) => utils.timeout(opts) > 0 ?
-        conn.execute(sql(opts, 'renew'), Date.now(), k) : undefined,
-    remove: (conn: FibKV.SqlConnection, opts: FibKV.FibKVOptions, k: string) => conn.execute(sql(opts, 'remove'), k),
+        conn.execute(sql(conn, opts, 'renew'), Date.now(), k) : undefined,
+    remove: (conn: FibKV.SqlConnection, opts: FibKV.FibKVOptions, k: string) => conn.execute(sql(conn, opts, 'remove'), k),
 };
